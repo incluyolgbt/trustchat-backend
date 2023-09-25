7c7a53d3b658978123b86d6c37a803d570a2f63a
estradae <estradae@ae.com>
2023-09-25 14:49:01 -0600

    corssss

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ let maxConnections = 1; // Número de chats permitidos menos 1 (en este caso 2)
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+}));
 
 const server = http.createServer(app);
 const io = new SocketServer(server, {
@@ -62,12 +64,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); 
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-})
 
 app.get('/', (req, res) => {
   res.send('hello');
